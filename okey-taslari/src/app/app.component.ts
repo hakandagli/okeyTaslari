6151605550ab4320e294662588e21f84b1d69030
hakandagli <hakan.dagli@arksoft.com.tr>
2022-08-20 15:13:51 +0300

    version1

--- okey-taslari/src/app/app.component.ts
+++ okey-taslari/src/app/app.component.ts
@@ -1,10 +1,61 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { card, colorType } from './models/card.model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+
+export class AppComponent implements OnInit {
   title = 'okey-taslari';
+  cards: card[] = [];
+  mobile:boolean = false;
+
+  ngOnInit(): void {
+    this.setCards();
+  }
+
+  setCards() {
+    let idCount = 0;
+    this.cards = [];
+    for (let color in colorType) {
+      if (!isNaN(Number(color))) {
+        for (let i = 1; i < 14; i++) {
+          idCount++;
+          this.cards.push(
+            {
+              id: idCount,
+              color: parseInt(color) as colorType,
+              value: i,
+              isSelected: false
+            }
+          )
+        }
+      }
+    }
+  }
+
+  selectedChange(card:card){
+    let index = this.cards.findIndex(x=>x.id===card.id);
+    this.cards[index].isSelected = !this.cards[index].isSelected;
+  }
+
+  get colorTypeKeys():colorType[]{  
+    let colors = [];
+    for (let color in colorType) {
+      if (!isNaN(Number(color))) {
+        colors.push(parseInt(color) as colorType);
+      }
+    }
+    return colors;
+  } 
+
+  get colorType(){
+    return colorType;
+  }
+
+  rotateChange(){
+    this.mobile = !this.mobile;
+  }
 }
---
+++ okey-taslari/src/app/models/card.model.ts
@@ -0,0 +1,15 @@
+export interface card {
+    id:number,
+    color:colorType,
+    value:number,
+    isSelected:boolean
+}
+
+export enum colorType {
+    red,
+    blue,
+    yellow,
+    black
+}
+  
+  
